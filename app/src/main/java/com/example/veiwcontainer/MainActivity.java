16520ad26225f21c113068981efad8d7ee27625a
sehyunf <ssam010925@gmail.com>
2026-05-27 09:27:18 +0900

    feat:slide image

--- app/src/main/java/com/example/veiwcontainer/MainActivity.java
+++ app/src/main/java/com/example/veiwcontainer/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.veiwcontainer;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart, btnStop;
+    ViewFlipper viewFlipper1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        viewFlipper1 = (ViewFlipper) findViewById(R.id.viewFlipper1);
+
+        viewFlipper1.setFlipInterval(1000);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewFlipper1.startFlipping();
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewFlipper1.stopFlipping();
+            }
         });
     }
 }
